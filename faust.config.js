012d3414257c3809c97b9a7a3af5a2348110735e
Joe Fusco <joe.fusco@wpengine.com>
2023-08-21 10:55:58 -0400

    Enable toolbar & custom plugin
    Signed-off-by: Joe Fusco <joe.fusco@wpengine.com>

--- faust.config.js
+++ faust.config.js
@@ -1,12 +1,14 @@
 import { setConfig } from "@faustwp/core";
 import templates from "./wp-templates";
 import possibleTypes from "./possibleTypes.json";
+import { CustomToolbarPlugin } from './plugins/CustomToolbarPlugin';
 
 /**
  * @type {import('@faustwp/core').FaustConfig}
  **/
 export default setConfig({
   templates,
-  experimentalPlugins: [],
+  experimentalPlugins: [new CustomToolbarPlugin()],
+  experimentalToolbar: true,
   possibleTypes,
 });
